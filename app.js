28346751bf318d620640bed480a20dea4e4fb2ee
Connor James Leech <connorleech@gmail.com>
2013-12-22 16:22:49 +0300

    create and delete todos

---
+++ app.js
@@ -0,0 +1,43 @@
+
+/**
+ * Module dependencies.
+ */
+require('./db');
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var engine = require('ejs-locals');
+
+var app = express();
+
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.engine('ejs', engine);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+
+app.use(express.bodyParser()); // add this before express.json()
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// routes
+app.get('/', routes.index);
+app.post('/create', routes.create);
+app.get('/destroy/:id', routes.destroy);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+var Todo = mongoose.model('Todo');
+
+// index route
+exports.index = function(req, res){
+    // query db for all todo items
+    Todo.find(function(err, todos, count){
+        res.render('index', { 
+            title: 'Express Todo app',
+            todos: todos // pass todo items to page
+        });
+    });
+};
+
+// create route
+exports.create = function(req, res){
+    new Todo({
+        content: req.body.content,
+        updated_at: Date.now()
+    })
+    .save(function(err, todo, count){
+        // redirect back to index page
+        res.redirect('/');
+    });
+};
+
+// destroy route
+exports.destroy = function(req, res){
+    Todo.findById(req.params.id, function(err, todo){
+        todo.remove(function(err, todo){
+            res.redirect('/');
+        });
+    });
+};
